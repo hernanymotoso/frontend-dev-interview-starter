641fa7d1b6b2230a994e8adfd1236d3a078bccc2
Jessica Huhnke <91915469+jhuhnke@users.noreply.github.com>
2025-08-24 13:46:45 -0500

    documentaton

--- src/hooks/useSolanaTransactions.tsx
+++ src/hooks/useSolanaTransactions.tsx
@@ -1,1 +1,3 @@
 // This is for the candidate to write 
+
+// Documentation: https://solana.com/docs/rpc/http/getsignaturesforaddress
--- src/hooks/useSuiTransactions.tsx
+++ src/hooks/useSuiTransactions.tsx
@@ -1,1 +1,3 @@
 // This is for the candidate to write 
+
+// Documentation: https://docs.sui.io/sui-api-ref#suix_querytransactionblocks
